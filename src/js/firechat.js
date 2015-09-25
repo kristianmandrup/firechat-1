9ea5beeed4e7f798c8ef45846b2fcf163b15ca0a
Jaganatha Rao Pemmadi <jagan579@gmail.com>
2015-09-25 20:11:12 +0530

    Fix for private chat inviation action for deafult chat/existing chat room #98

--- src/js/firechat.js
+++ src/js/firechat.js
@@ -320,7 +320,10 @@
 
       // Skip if we're already in this room.
       if (self._rooms[roomId]) {
-        return;
+        //return;
+
+        //this is a hack to make the disabled action(private chat invite and block symobls visible on first sign in)
+        self.leaveRoom(roomId);
       }
 
       self._rooms[roomId] = true;
